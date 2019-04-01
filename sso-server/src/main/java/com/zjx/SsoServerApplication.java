8f7e678a63b2f43fde8a4a7f075280d55b519233
arsenalzjx <328036444@qq.com>
2019-04-01 13:47:50 +0800

    创建SSO项目并完成相关配置

---
+++ sso-server/src/main/java/com/zjx/SsoServerApplication.java
@@ -0,0 +1,15 @@
+package com.zjx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zjx
+ * @date: 2019/3/28 15:37
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+}
---
+++ sso-server/src/main/java/com/zjx/sso/server/SsoAuthorizationServerConfig.java
@@ -0,0 +1,59 @@
+package com.zjx.sso.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * @author: zjx
+ * @date: 2019/3/28 15:39
+ */
+@Configuration
+@EnableAuthorizationServer
+public class SsoAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("zjx1")
+                .secret("zjxsecret1")
+                .authorizedGrantTypes("authorization","refresh_token","authorization_code")
+                .scopes("all","read","write")
+                .and()
+                .withClient("zjx2")
+                .secret("zjxsecret2")
+                .authorizedGrantTypes("authorization","refresh_token","authorization_code")
+                .scopes("all","read","write");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(jwtTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore(){
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("zjx");
+        return converter;
+    }
+
+}
